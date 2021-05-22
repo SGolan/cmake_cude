eeae675984742c135a1492a485c991bd0ad8e363
Sharon Golan <sharon.golan@leddartech.com>
2021-05-22 14:34:45 +0300

    changes in cuda-related CMakeLists.txt files (2 of them) to enable generation & build in Windows (VSCode). Should be verified in Linux and also back in Windows using VS2019.

--- cmake_cuda_example/main.cpp
+++ cmake_cuda_example/main.cpp
@@ -4,11 +4,13 @@
 using std::cout;
 using std::endl;
 
+constexpr int C_DATA_SIZE = 1000000;
+
 
 int main()
 {
     // allocate and initialize a vector
-    float *v = new float [1000000];
+    float *v = new float [C_DATA_SIZE];
     for(int i=0; i<1e6; i++)
         v[i] = i;
 
@@ -19,16 +21,16 @@ int main()
        exit(1);
 
     // verify the result
-    for(int i=0; i<1e6; i++)
+    for(int i=0; i<C_DATA_SIZE; i++)
     {
         if(v[i] != i+5) 
         {  
-            cout << "Error : (v[" << i << "]=" << v[i] << ") != " << i+5;
+            cout << "-vv- Error : (v[" << i << "]=" << v[i] << ") != " << i+5;
             exit(2);
         } 
         
     }
 
-    cout << "Done" << endl;
+    cout << "-vv- Done" << endl;
     return 0;
 }
